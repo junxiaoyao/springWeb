7ab0e1b3a741c8f5da558c73519d7452686d1245
ybl <1162474984@qq.com>
2019-01-08 13:51:35 +0800

    添加spring security依赖

---
+++ new/src/main/java/com/jxy/springMvc/configs/MyDelegatingFilter.java
@@ -0,0 +1,7 @@
+package com.jxy.springMvc.configs;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class MyDelegatingFilter extends AbstractSecurityWebApplicationInitializer {
+
+}
